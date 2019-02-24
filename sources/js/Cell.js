cca6f2c70bf1456259e62273b2fa5ce3b9f77180
Wanderson Silva <wandersonsilva768@gmail.com>
2019-02-24 05:00:50 -0300

    initial commit

---
+++ sources/js/Cell.js
@@ -0,0 +1,12 @@
+
+
+class Cell {
+	constructor (attributes) {
+		this.x         = attributes.x;
+		this.y         = attributes.y;
+		this.intensity = attributes.intensity;
+	}
+
+	draw (drawRect) {}
+}
+
